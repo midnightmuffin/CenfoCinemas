5e47ec328edc2760cf0e80cb61ff983fc592a4be
Fabiola Zuñiga <fzunigav@ucenfotec.ac.cr>
2025-06-21 19:00:43 -0600

    Cambios en Pogram.cs WebAPI

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -11,10 +11,10 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 /*if (app.Environment.IsDevelopment())
-{
+{*/
     app.UseSwagger();
     app.UseSwaggerUI();
-}*/
+//}
 
 app.UseHttpsRedirection();
 
